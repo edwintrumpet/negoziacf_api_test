0d1522639ab68809c036a7d4692ec8f858759595
Edwin Garcia <spark.com.co@gmail.com>
2021-07-07 17:21:23 -0500

    [feature] set error handler and catch 404 middlewares

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ const debug = require('debug')('app:server');
 
 const { port } = require('./config');
 const routes = require('./routes');
+const { logErrors, wrapErrors, errorHandler } = require('./middlewares/errorHandlers');
+const notFoundHandler = require('./middlewares/notFoundHandler');
 
 // Initializations
 const app = express();
+app.use(express.json());
 
 // Routes
 routes(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Handle errors
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 // Server
 app.listen(port, (err) => {
